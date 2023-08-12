2d81131108913699a7cdeb635fe01b2ffe849120
Abdur Rashid Sawom <asawom250@gmail.com>
2023-08-12 20:23:36 +0600

    Update settings.py

--- My_Social_Project/settings.py
+++ My_Social_Project/settings.py
@@ -81,12 +81,12 @@ WSGI_APPLICATION = 'My_Social_Project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
