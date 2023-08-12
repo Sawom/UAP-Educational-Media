bf0c59252854f28dff57e31b559931a665615dd9
Abdur Rashid Sawom <asawom250@gmail.com>
2023-08-12 19:52:45 +0600

    Update wsgi.py

--- My_Social_Project/wsgi.py
+++ My_Social_Project/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'My_Social_Project.settings')
 
 application = get_wsgi_application()
+app = application
